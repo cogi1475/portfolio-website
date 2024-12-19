c5459b5f00eae55e0de5e08eaa63a56a652fd708
YeBeen <jybinj4@gmail.com>
2024-12-19 16:29:12 +0900

    3rd

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -17,10 +17,20 @@ const Home = () => {
       </div>
 
       <h1 className="text-4xl md:text-5xl font-extrabold text-white mb-10">
-        안녕하세요 저는 정예빈입니다. 학번 : 92313635 전공 : 정보보호학과
+        안녕하세요 저는 정예빈입니다.
       </h1>
 
-      <p className="text-lg md:text-xl max-w-2xl mx-auto text-white leading-relaxed"></p>
+      <p className="text-lg md:text-xl max-w-2xl mx-auto text-white leading-relaxed">
+        학번 : 92313635
+        <br />
+        전공 : 정보보호학과
+        <br />
+        취미 : 독서, 웹서핑, 전자기기 정보 정리
+        <br />
+        좋아하는 것 : 책, 전자기기, 친구들과의 시간
+        <br />
+        싫어하는 것 : 시끄러운 소음
+      </p>
     </section>
   )
 }
